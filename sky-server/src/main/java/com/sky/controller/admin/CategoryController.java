be691c799c7838369f3f1b52bb19f939e0200f31
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-08-19 00:45:41 +0800

    feat: Add pagination for category queries
    - Implemented pagination functionality for querying categories
    - Added API support for pagination parameters (page number, page size) in category queries
    - Corrected the path of the "Category" interface

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -1,23 +1,22 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.CategoryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author ZZHow
  * @date 2024/8/18
  */
 @RestController
-@RequestMapping("/admin/setmeal")
+@RequestMapping("/admin/category")
 @Slf4j
 @Api(tags = "分类相关接口")
 public class CategoryController {
@@ -39,5 +38,21 @@ public class CategoryController {
         categoryService.save(categoryDTO);
 
         return Result.success();
+    }
+
+    /**
+     * 分类分页查询
+     *
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("分类分页查询")
+    public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO) {
+        log.info("分类分页查询：categoryPageQueryDTO = {}", categoryPageQueryDTO);
+
+        PageResult pageResult = categoryService.pageQuery(categoryPageQueryDTO);
+
+        return Result.success(pageResult);
     }
 }
--- sky-server/src/main/java/com/sky/service/CategoryService.java
+++ sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.result.PageResult;
 
 /**
  * @author ZZHow
@@ -14,4 +16,12 @@ public interface CategoryService {
      * @param categoryDTO
      */
     void save(CategoryDTO categoryDTO);
+
+    /**
+     * 分类分页查询
+     *
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
 }
