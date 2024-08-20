c1f23c80e614f8caf192adccce61f5c12224f2f3
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-08-20 17:30:22 +0800

    feat: Use AOP to automatically populate common fields
    - Implemented Aspect-Oriented Programming (AOP) to handle automatic population of common fields such as createTime, updateTime, createUser, and updateUser
    - Added aspects that intercept entity operations to inject values into common fields
    - Ensured that the AOP solution works across various entities and operations (e.g., create, update)

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.Autofill;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -17,6 +19,12 @@ import java.util.List;
 @Mapper
 public interface CategoryMapper {
 
+    /**
+     * 新增分类
+     *
+     * @param category
+     */
+    @Autofill(value = OperationType.INSERT)
     @Insert("insert into category (type, name, sort, status, create_time, update_time, create_user, update_user)" +
             " values (#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Category category);
@@ -34,6 +42,7 @@ public interface CategoryMapper {
      *
      * @param category
      */
+    @Autofill(value = OperationType.UPDATE)
     void update(Category category);
 
     /**
